8f16693a0804f4e263dd2c43829b45791b85807b
doomdybbuk <manan.jain16949@gmail.com>
2025-02-03 18:39:00 +0530

    Enhance build script to install gdown and download best.pt model

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Install gdown if not installed
+pip install gdown
+
+# Create necessary directories
+mkdir -p scripts/blood_cell_detection/blood_cell_detection7/weights
+
+# Download best.pt from Google Drive
+gdown --id 1c2VAdw1AQnDg4mlK8l_e_NthdA1Jg_G3 -O scripts/blood_cell_detection/blood_cell_detection7/weights/best.pt
